3c1750d94dd97f43dd3e2fa99ff593c4611078a6
Mosho <moises.leon@chasacademy.se>
2025-09-15 15:59:53 +0200

    dagens lektion om JS

--- js/conditions.js
+++ js/conditions.js
@@ -3,9 +3,51 @@ const person = {
     age: "43"
 }
 
-const isAdult = true
+//const isAdult = true
+const harDuFragatMamma = true
+const hasTicket = false
 
+//Boolensk uttryck
+
+
+// Boolensk uttryck
+const isAdult = person.age  >= 18 // Ger sant eller falsk
+
+// Om du är större eller lika med 15 år och mindre än 18 år.
+const inTeenager = person.age >= 15 && person.age < 18
+
+/*
+if (!isAdult) {
+    //Kör den här koden om isAdult är sant
+    console.log("Välkommen gubbe!")
+} else if (!harDuFragatMamma) {
+    console.log("Bra att du frågat mamma! men du är inte så gammal")
+}else {
+    console.log("Detta blev else satsen")
+}
+*/
+/*
+if (isAdult && (harDuFragatMamma || hasTicket)) {
+    //Kör den här koden om isAdult är sant
+    console.log("Välkommen gubbe! du uppfyller alla krav")
+}else {
+    console.log("Du uppfyller inte alla krav")
+}
+*/
+
+/*
 if (isAdult) {
     //Kör den här koden om isAdult är sant
     console.log("Välkommen gubbe!")
 }
+*/
+
+/* 
+if (!isAdult) {
+    //Kör den här koden om isAdult är sant
+    console.log("Du är inte så gammal!")
+}
+    */
+
+// && = And
+// || = Or
---
+++ js/null.js
@@ -0,0 +1,13 @@
+const amount = "" //
+
+/* if (!amount) {
+    console.log("Kous")
+} */
+
+/*
+
+null = ingen värde
+undefined = aldrig fått någon värde
+NaN = Not a number
+
+*/

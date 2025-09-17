71af65ed906ef4780a40126a65007d65c59279a1
Mosho <moises.leon@chasacademy.se>
2025-09-17 11:02:01 +0200

    Second part av workshop 2025-09-17

--- js/funtions.js
+++ js/funtions.js
@@ -4,10 +4,18 @@ Version: 0.1
  */
 console.log("======== JS ========" + "Genomgång av funktioner - 2025-09-17"+"\n" )
 
-function add(a, b) { // det som finns innanför parantesen kallas för parametrar som är beredda att ta emot information till funktionen.
+/* function add(a, b) { // det som finns innanför parantesen kallas för parametrar som är beredda att ta emot information till funktionen.
     const result = a + b //Deklarerar variabeln 
     console.log(result) // Printar resultaten av variabel a + b
 }
 
-add(5, 10)
-add (2, 8)
+add(5, 10) // Du lägger till data i parametrarna.
+add (2, 8) */
+
+function nameFunction(a, b) { // det som finns innanför parantesen kallas för parametrar som är beredda att ta emot information till funktionen.
+    const sum = a + b //Deklarerar variabeln 
+    console.log(sum) // Printar resultaten av variabel a + b
+    return sum
+}
+
+const sum = nameFunction(10, 5) //Variabeln sparar resultat av sum. "Du får kakan efter du har bakat i en låda"
---
+++ js/string.js
@@ -0,0 +1,14 @@
+/* Author: Mos 
+Date: 2025-09-17
+Version: 0.1
+ */
+
+console.log("" )
+
+const myName = "Mos"
+const age = 35
+const message = "Jag heter Mos och är 44år gammal"
+const messageDynamicOld = "Jag heter" + myName + "och jag är" + age + "år gammal"
+const messageDynamic = `Jag heter ${myName} och jag är ${age} år gammal`
+
+// Backticks ```<--- tillåter dig skriva ${} för att föra in JS.

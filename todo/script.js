4565d3920470159a13f3f53f328a6683e0d15ea0
Mosho <moises.leon@chasacademy.se>
2025-09-24 17:56:26 +0200

    From bookis till studio

--- todo/script.js
+++ todo/script.js
@@ -25,22 +25,24 @@ const addTask = () => {
     //Denna Variabel skapar en HTML element <li>
     const listItem = document.createElement("li")
 
-    const listItemContainer = document.createElement("div").classList("test")
+    //Skapar en div inuti <li> med en class
+    const listItemContainer = document.createElement("div")
+    listItemContainer.classList.add("listItemContainer")
 
+    //Skapar en ta bort knapp
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("deleteBtn")
+    deleteBtn.textContent = "Ta bort"
 
-    //listItem.textContent = todoName
+    listItemContainer.textContent = todoName
 
     //Ren HTML
     //listItem.innerHTML = todoName + deleteBtn
 
     //Lägg till i listan
-    taskList.appendChild(listItem)
-
-    
+    taskList.appendChild(listItemContainer)
 
     //Rensar input listan
     inputField.value = ""
-
-
 
 }
